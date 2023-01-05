35177af02c8c134b99923bf0843a8262aced5805
alexxtomas <atomasllimos@gmail.com>
2023-01-05 15:51:51 +0100

    fix: Remove unnecessary console.log

--- src/commons/clone.ts
+++ src/commons/clone.ts
@@ -3,7 +3,6 @@ import { GLOBALSTATE, REACTSTATE } from './states.js'
 
 const cloneTemplate = async (): Promise<void> => {
   if (REACTSTATE) {
-    console.log(REACTSTATE)
     const downloader = degit(
       `acceleratorProject/templates-react/${REACTSTATE?.variant}/acc-${
         REACTSTATE?.styles
@@ -14,7 +13,7 @@ const cloneTemplate = async (): Promise<void> => {
         force: true
       }
     )
-    downloader.clone(GLOBALSTATE.projectName).then(() => {
+    await downloader.clone(GLOBALSTATE.projectName).then(() => {
       console.log(`Repositorio descargado en ${GLOBALSTATE.projectName}`)
     })
   }
